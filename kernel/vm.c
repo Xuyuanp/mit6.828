2f1f7cad7fd16271b9de4ede29a1cdb8781a4b57
Xuyuan Pang <xuyuanp@gmail.com>
2020-10-27 19:54:51 +0800

    lab 3: pte print

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,28 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+_vmprint_pte(pagetable_t pagetable, int level)
+{
+  int l;
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      for (l = 0; l < level; l++)
+          printf(" ..");
+      printf("%d: pte %p pa %p\n", i, pte, child);
+      if (level <= 3) _vmprint_pte((pagetable_t)child, level+1);
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+    printf("page table %p\n", pagetable);
+    _vmprint_pte(pagetable, 1);
+}
