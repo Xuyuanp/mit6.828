f462156bc88bcac720c7e31701a118f56d483c50
Xuyuan Pang <xuyuanp@gmail.com>
2020-11-13 19:11:17 +0800

    multithreading: ph

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -8,6 +8,8 @@
 #define NBUCKET 5
 #define NKEYS 100000
 
+pthread_mutex_t lock;
+
 struct entry {
   int key;
   int value;
@@ -48,10 +50,14 @@ void put(int key, int value)
   }
   if(e){
     // update the existing key.
+    pthread_mutex_lock(&lock);
     e->value = value;
+    pthread_mutex_unlock(&lock);
   } else {
     // the new is new.
+    pthread_mutex_lock(&lock);
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock);
   }
 }
 
@@ -99,6 +105,8 @@ get_thread(void *xa)
 int
 main(int argc, char *argv[])
 {
+  pthread_mutex_init(&lock, NULL);
+
   pthread_t *tha;
   void *value;
   double t1, t0;
